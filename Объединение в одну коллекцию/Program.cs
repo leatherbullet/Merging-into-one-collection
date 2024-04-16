dcab7fbdebed3d0c89fed6eed4803aef26413aab
leatherbullet <al.obnizov@yandex.ru>
2024-04-16 09:19:48 +0300

    Добавьте файлы проекта.

---
+++ Объединение в одну коллекцию/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Объединение_в_одну_коллекцию
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "1", "2", "1" };
+            string[] array2 = { "3", "2" };
+            List<string> collection = new List<string>();
+
+            AddItem(array1, collection);
+            AddItem(array2, collection);
+
+            Show(collection);
+        }
+
+        static void Show(List<string> collection)
+        {
+            foreach (string number in collection)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+
+        static void AddItem(string[] array, List<string> collection)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (collection.Contains(array[i]) == false)
+                {
+                    collection.Add(array[i]);
+                }
+            }
+        }
+    }
+}
